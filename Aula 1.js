1e9e0b8245514ca173da7e6f8708b146a742f63a
Kauansinho014_kkk <168597010+Kauansinho015@users.noreply.github.com>
2024-05-08 06:45:10 -0300

    Add files via upload

---
+++ Aula 1.js
@@ -0,0 +1,24 @@
+let a=1
+a=2
+//troca do valor de "a"
+
+let b=3
+b=5
+//troca do valor de b
+
+c=b
+b=a
+a=b
+a=c 
+
+//"a"recebe o valor de "b" e "b" o valor de "a"
+
+console.log(a)
+//exibe o novo valor de "a"
+
+console.log(b)
+//exibe o novo valor de b
+
+console.log(a+b)
+//exibe o valor de "a" mais o valor de "b"
+
---
+++ Aula2.js
@@ -0,0 +1,19 @@
+const cores=['azul','amarelo','verde','laranja','roxo']
+//lista de dados (array).
+
+console.log(cores)
+//mostra todos arquivos do array.
+
+const paises=['china','australia','japão','colombia','brasil']
+
+console.log(paises[4])
+//exibe um elemento especifico atraves do numero do item do array que começa de 0 ao Infinito.
+
+console.log('tamanhodo array:'+paises.length)
+//mostrao tamanho do array,ou a quantidade de items.e o mais junta as palavras,numeros,items... 
+
+console.push('usa')
+//adiciona um elemento
+
+console.pop('china')
+//tira um elemento
