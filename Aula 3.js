aab6e21fdddbbbfd8dea6abdbe210d03dc41e0ad
Kauansinho014_kkk <168597010+Kauansinho015@users.noreply.github.com>
2024-05-10 16:33:13 -0300

    Add files via upload

---
+++ Aula 3.js
@@ -0,0 +1,29 @@
+//parte 1
+
+const minhaidade = 15
+
+
+if(minhaidade == 15){
+    console.log('resposta :'+ ' correta')
+}
+else{
+  
+ console.log('resposta :'+' incorreta!!')
+  
+}
+
+
+
+//parte 2
+
+const numero = 100000
+
+if(numero == 10900){
+  
+    console.log('resposta'+'correta')
+  
+}else{
+  
+    console.log('resposta :'+' incorreta')
+  
+}
