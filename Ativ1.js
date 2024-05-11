5d24cd61565c46b78fd3a189bcd8b431e5c873fc
Kauansinho014_kkk <168597010+Kauansinho015@users.noreply.github.com>
2024-05-11 00:44:24 -0300

    Add files via upload

---
+++ Ativ1.js
@@ -0,0 +1,9 @@
+//percorrer uma lista de nome Contendo os nomes: Eduardo, maria, joão e francisco, Numero da execução começado em 1
+//nome que esta sendo executado
+//Separadores
+
+const nomes = ['Eduardo', 'maria', 'joão', 'francisco'];
+nomes.forEach(elemento => {
+  console.log(elemento) ;
+  console.log('_____________________________________');
+});
---
+++ Aula4.js
@@ -0,0 +1,32 @@
+console.log('parte1')
+
+//Estruturas de repetiçao
+
+//tipos
+//for "normal
+//repetiçao por Array
+
+//Estrutura do forEach
+
+//nomedoArray.forEach(nome da variavel const =>{
+  
+//})
+
+//for normal:
+
+const cidade = ['ipaussu','chavantes','canitar']
+
+cidade.forEach(elemento =>{
+  
+  console.log(elemento);
+  
+})
+
+
+//for normal tambem
+
+const cores = ['azul', 'verde', 'preto'];
+
+cores.forEach(elemento => {
+  console.log(elemento);
+});
